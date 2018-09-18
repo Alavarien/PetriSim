e69fc6fe28a24778b8e59c01e9bd26fcdd93ea04
Laurent Pichollet <lpicholl@enssat.fr>
2018-09-18 02:15:37 +0200

    ...

---
+++ PetriSim/Cells/BasicCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriSim.Cells
+{
+    class BasicCell
+    {
+        private struct Position
+        {
+            public int x;
+            public int y;
+        }
+
+        private int health;
+
+
+    }
+}
